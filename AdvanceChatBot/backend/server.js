86b247ae4fd8fca404975c129150a4319f6e9558
Shahid321fw11 <sidansari20@gmail.com>
2023-10-31 16:27:57 +0530

    add get request for checking

--- AdvanceChatBot/backend/server.js
+++ AdvanceChatBot/backend/server.js
@@ -67,6 +67,10 @@ app.post("/chat", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Working fine");
+});
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
